0b4a82bbc57195125cd5505400162c93f60214be
nathan-m-schneider-22 <nathan.m.schneider.22@dartmouth.edu>
2023-05-30 20:29:34 -0700

    added support for profiler

--- cryptoquip/cryptoquip_solver.py
+++ cryptoquip/cryptoquip_solver.py
@@ -6,6 +6,18 @@ import time
 import re
 from queue import PriorityQueue
 import sys
+from line_profiler import LineProfiler
+
+profiler = LineProfiler()
+
+
+def profile(func):
+    def inner(*args, **kwargs):
+        profiler.add_function(func)
+        profiler.enable_by_count()
+        return func(*args, **kwargs)
+
+    return inner
 
 
 letters = [l for l in string.ascii_uppercase]
